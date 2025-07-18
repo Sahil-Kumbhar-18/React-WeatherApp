f51a89245c01e14aa171310d6ea4fb5602664b26
unknown <salilkumbhar1011@gmail.com>
2025-07-18 22:47:06 +0530

    added few data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import SearchBox from './SearchBox'
 import './App.css'
-
+import Button from './Button';
 function App() {
   
 
   return (
-    <>
+    <div className='container '>
       <SearchBox/>
-    </>
+    
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { useState } from 'react';
 import './App.css'
+import Button from './Button';
 export default function SearchBox() {
-    const [city, setEmail] = useState("");
-
+    const [city, setCity] = useState("");
+    let handelSumbit=(e)=>{
+      e.preventDefault();
+      console.log(city)
+      setCity("")
+    }
     return (
+      <form onSubmit={handelSumbit}>
       <div className="wrapper">
+      <h3>Search For Weather</h3>
         <div className={`custom-textfield ${city ? "filled" : ""}`}>
           <input
-            type="email"
+            type="text"
             id="email"
             value={city}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setCity(e.target.value)}
             required
           />
-          <label htmlFor="email">city</label>
-        </div>
-      </div>
+           <label htmlFor="email">city</label>
+           <br />
+           <Button/>
+         </div>
+       </div>
+      </form>
     );
 }
-
